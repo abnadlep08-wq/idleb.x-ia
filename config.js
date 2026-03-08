2ad056ade56d4fc7585bed7ffbf7bb988146d77e
abnadlep08-wq <abnadlep08@gmail.com>
2026-03-08 07:46:54 +0300

    Create config.js

---
+++ config.js
@@ -0,0 +1,17 @@
+// ملف الإعدادات
+const CONFIG = {
+    // Supabase (منصة مجانية لقواعد البيانات والمصادقة)
+    supabaseUrl: "https://dlspfrbtbcxwfjmidjnc.supabase.co",
+    supabaseKey: "sb_publishable_97g8HUlw4YHcIUZ6ciltsA_PkAGRr5W",
+    
+    // الصور
+    heroBackground: "https://i.ibb.co/d4DfRnH2/Pookie-Spide-WEBP-0.jpg",
+    
+    // معلومات الموقع
+    siteName: "Idleb X IA",
+    
+    // API Keys (سيتم تعبئتها من Netlify Environment Variables)
+    magicHourApiKey: "mhk_live_ZnPCEcaXww2XIhgvJUMhRHOhbXCEWZqHETWY5os85mfx0hfMRuU3CDMRx75op3l4U8sjqG2Fqd0LnKR9"
+};
+
+window.CONFIG = CONFIG;
